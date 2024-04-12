75beebfa23a20551c7bc3a5f5b22e97e0617ce7e
Mark M <chtozamm@gmail.com>
2024-04-12 20:01:43 +0300

    Manage blog content with Content Collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss"
+import rss from "@astrojs/rss"
+import { getCollection } from "astro:content"
 
 export async function GET(context) {
+  const posts = await getCollection("posts")
   return rss({
     title: "Astroblog 🚀",
     description: "Personal *Astronomical* journey to obtain knowledge",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+    })),
     customData: `<language>en-us</language>`,
   })
 }
